d1fa1d917e6d6f44d38f3d503cd00e503b6055b6
DESKTOP-0NQH1K7\user <queistion@gmail.com>
2024-06-06 01:49:25 +0900

    include 추가

--- PipelineService/src/util.cpp
+++ PipelineService/src/util.cpp
@@ -1,6 +1,7 @@
 #include <luna-service2/lunaservice.h>
 #include <pbnjson.h>
 #include <PmLog.h>
+#include <PmLogLib.h>
 #include "util.h"
 
 bool parse_ls_message(LSMessage *message, const char *url, bool *od, int *gl_effect) {
