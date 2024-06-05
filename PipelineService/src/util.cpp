87ddc00c8474fb5014532773f193e060127baaae
DESKTOP-0NQH1K7\user <queistion@gmail.com>
2024-06-06 01:50:44 +0900

    헤더파일 include 추가

--- PipelineService/src/util.cpp
+++ PipelineService/src/util.cpp
@@ -1,7 +1,6 @@
 #include <luna-service2/lunaservice.h>
 #include <pbnjson.h>
 #include <PmLog.h>
-#include <PmLogLib.h>
 #include "util.h"
 
 bool parse_ls_message(LSMessage *message, const char *url, bool *od, int *gl_effect) {
--- PipelineService/src/util.h
+++ PipelineService/src/util.h
@@ -1,6 +1,11 @@
 #ifndef __UTIL_H__
 #define __UTIL_H__
 
+#include <luna-service2/lunaservice.h>
+#include <pbnjson.h>
+#include <PmLog.h>
+#include "util.h"
+
 bool parse_ls_message(LSMessage *message, const char *url, bool *od, int *gl_effect);
 static PmLogContext getPmLogContext();
 
