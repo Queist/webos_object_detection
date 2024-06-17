a413dc80592d34e299763124c731a48744a66099
DESKTOP-0NQH1K7\user <queistion@gmail.com>
2024-06-17 21:11:18 +0900

    로그 다시 추가

--- PipelineService/src/pipeline/pipeline.cpp
+++ PipelineService/src/pipeline/pipeline.cpp
@@ -69,6 +69,7 @@ GstElement *init_sink_bin() {
 
 int objectDetectionPipeline(const char *url, bool use_object_detection, int gl_effect) {
     PmLogInfo(getPmLogContext(), "GSTREAMER_PIPELINE", 0, "pipeline called");
+    PmLogInfo(getPmLogContext(), "GSTREAMER_PIPELINE", 0, PMLOGKFV("ErrorElement", "%d", 5));
     PmLogInfo(getPmLogContext(), "GSTREAMER_PIPELINE", 0, PMLOGKFV("ErrorElement", "%s", url));
 
     // Elements declaration
